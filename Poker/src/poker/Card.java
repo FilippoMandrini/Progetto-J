b358134dc7cc259da6c9aa4ef1881a6a2d509201
Ludovico <Ludovico@NotebookAir>
2017-04-26 12:35:41 +0200

    Inserita bozza di Javadoc nelle classi "Card" e "Deck"

--- Poker/src/poker/Card.java
+++ Poker/src/poker/Card.java
@@ -2,6 +2,7 @@ package poker;
 
 import java.util.Objects;
 
+
 public class Card implements Comparable {
 
     private int value;
@@ -10,19 +11,37 @@ public class Card implements Comparable {
     private static final String valueName[] = {"Due", "Tre", "Quattro", "Cinque", "Sei",
         "Sette", "Otto", "Nove", "Dieci", "Fante", "Regina", "Re", "Asso"};
 
+    /**
+     *
+     * @Ritorna il valore numerico della Carta
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     *
+     * 
+     * @Ritorna la Stringa corrispondente al valore della Carta
+     */
     public static String getValueName(int value) {
         return valueName[value];
     }
     
+    /**
+     *
+     * @param seed
+     * @return
+     */
     public static String getSeedName(Seed seed) {
         
         return seedName[seed.getValue()];
     }
 
+    /**
+     *
+     * @Ritorna il seme della Carta
+     */
     public Seed getSeed() {
         return seed;
     }
@@ -31,6 +50,7 @@ public class Card implements Comparable {
         this.value = value;
         this.seed = seed;
     }
+
 
     public Card(int value) {
         this.value = value;
--- Poker/src/poker/Deck.java
+++ Poker/src/poker/Deck.java
@@ -3,10 +3,14 @@ package poker;
 import java.util.ArrayList;
 import java.util.Random;
 
+
 public class Deck {
     
     private ArrayList<Card> mazzo;
   
+    /**
+     *
+     */
     public Deck() {
         this.mazzo = new ArrayList();
         for(int i=0; i<4 ; i++){
@@ -16,6 +20,10 @@ public class Deck {
         }
     }
     
+    /**
+     *
+     * @Metodo che estrae una carta dal mazzo e successivamente la rimuove dal mazzo, evitando duplicati
+     */
     public Card chooseCard() {
         Random random = new Random();
         Card cartaEstratta = mazzo.get(random.nextInt(mazzo.size()));
@@ -23,6 +31,10 @@ public class Deck {
         return cartaEstratta;
     }
 
+    /**
+     *
+     * @Ritorna un arraylist del mazzo
+     */
     public ArrayList<Card> getMazzo() {
         return mazzo;
     }
