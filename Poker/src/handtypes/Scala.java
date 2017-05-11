a7d9a31030545100cc7e2ba719fe9df68d446d15
Luca Stanzani <Nickelsilver@10.87.171.56>
2017-05-11 12:17:22 +0200

    Corretto toString in caso di scala al 5

--- Poker/src/handtypes/Scala.java
+++ Poker/src/handtypes/Scala.java
@@ -8,18 +8,20 @@ public class Scala extends Hand {
     public Scala(ArrayList<Card> cards) {
         super(cards);
     }
-    
+
     @Override
     public double getPoints() {
-        if(cards.get(0).getValue() == 12 && cards.get(1).getValue() == 3){
+        if (cards.get(0).getValue() == 12 && cards.get(1).getValue() == 3) {
             return 400 + this.cards.get(1).getValue();
         }
         return 400 + this.cards.get(0).getValue();
     }
-    
+
     @Override
-    public String toString()
-    {
+    public String toString() {
+        if (cards.get(0).getValue() == 12 && cards.get(1).getValue() == 3) {
+            return "Scala al " + Card.getValueName(this.cards.get(1).getValue());
+        }
         return "Scala al " + Card.getValueName(this.cards.get(0).getValue());
     }
 }
