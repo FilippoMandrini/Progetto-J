ff17c16f2392d74254a572b20a986779b15198b3
Filippo Mandrini <FilippoMandrini@10.87.221.87>
2017-05-20 17:28:40 +0200

    Prima versione di test delle classi Client e Server con connessione.

--- Poker/src/client/Client.java
+++ Poker/src/client/Client.java
@@ -1,7 +1,8 @@
 package client;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -9,20 +10,41 @@ public class Client {
     
     public static void main(String[] args) throws IOException {
         
+        Scanner in = null;
+        PrintStream out = null;
+        Socket socket = null;
         final int port = 7777;
         final String ip = "192.168.1.101";
+        String testMessage;
         
-        Socket client = new Socket(ip, port);
-        
-        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-        Scanner in = new Scanner(client.getInputStream());
-        
-        Scanner tastiera = new Scanner(System.in);
-        while(true) {
-            String input = tastiera.nextLine();
-            out.println(input);
-            String output = in.nextLine();
-            System.out.println(output);
+        try {
+            socket = new Socket(ip, port);
+            in = new Scanner(new InputStreamReader(socket.getInputStream()));
+            out = new PrintStream(socket.getOutputStream(), true);
+            testMessage = in.nextLine();
+            System.out.println("Messaggio ricevuto: " + testMessage);
+            out.close();
+            in.close();
         }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
     }
 }
+        
+//        final int port = 7777;
+//        final String ip = "192.168.1.101";
+//        
+//        Socket client = new Socket(ip, port);
+//        
+//        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+//        Scanner in = new Scanner(client.getInputStream());
+//        
+//        Scanner tastiera = new Scanner(System.in);
+//        while(true) {
+//            String input = tastiera.nextLine();
+//            out.println(input);
+//            String output = in.nextLine();
+//            System.out.println(output);
+//        }
--- Poker/src/server/Server.java
+++ Poker/src/server/Server.java
@@ -1,27 +1,103 @@
 package server;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
-   
-public class Server {
+
+public class Server extends Thread {
     
-    public static void main(String[] args) throws IOException {
+    private ServerSocket server;
+    final int port = 7777;
+    
+    public static void main(String[] args) throws IOException, Exception {
         
-        final int port = 7777;
+        new Server();
+    }
+    
+    public Server() throws Exception {
         
-        ServerSocket server = new ServerSocket(port);
-        Socket client = server.accept();
-        
-        Scanner in = new Scanner(client.getInputStream());
-        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+        this.server = new ServerSocket(this.port);
+        this.start();
+    }
+    
+    public void run() {
         
         while(true) {
-            String riga = in.nextLine();
-            out.println("Ricevuto " + riga);
-            out.flush();
+            try {
+                Socket client = server.accept();
+                Connect c = new Connect(client);
+            } 
+            catch (IOException ex1) {
+            }
+        }
+    }
+    
+    class Connect extends Thread {
+        
+        private Socket client = null;
+        Scanner in = null;
+        PrintStream out = null;
+        
+        public Connect(Socket clientSocket) throws IOException {
+            
+            this.client = clientSocket;
+            
+            try {
+                in = new Scanner(new InputStreamReader(client.getInputStream()));
+                out = new PrintStream(client.getOutputStream(), true);
+            }
+            catch (IOException ex2) {
+                try {
+                    client.close();
+                }
+                catch(Exception ex3) {
+                    System.out.println(ex3.getMessage());
+                }
+                return;
+            }
+            this.start();
+        }
+        
+        public void run() {
+            try {
+                out.flush();
+                out.close();
+                in.close();
+                client.close();
+            }
+            catch(Exception ex3) {   
+            }
         }
     }
 }
+
+//        int clientNumber = 0;
+//        final int port = 7777;
+//        
+//        ServerSocket server = new ServerSocket(port);
+//        Socket client = server.accept();
+//        
+//        Scanner in = new Scanner(client.getInputStream());
+//        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+//        
+//        while(true) {
+//            String riga = in.nextLine();
+//            out.println("Ricevuto " + riga);
+//            out.flush();
+//        }
+//    }
+//    
+//    private static class Supporto extends Thread {
+//        
+//        private Socket socket;
+//        private int clientNumber;
+//        
+//        public Supporto(Socket socket, int clientNumber) {
+//            this.socket = socket;
+//            this.clientNumber = clientNumber;
+//            log("Nuova connessione con client # " + clientNumber + " presso il socket " + socket);
+//        }
+//    }
