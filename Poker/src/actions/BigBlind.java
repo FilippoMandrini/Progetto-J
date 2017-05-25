5be9f40c2117446a2b173244925c4d542921978a
Luca Stanzani <Nickelsilver@10.87.171.56>
2017-05-25 12:15:02 +0200

    Aggiunte azioni big e small blind e azione generica di test

---
+++ Poker/src/actions/BigBlind.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actions;
+
+/**
+ *
+ * @author Nickelsilver
+ */
+ public class BigBlind extends Action {
+
+    public BigBlind(int amount) {
+        super("Big Blind", "paga big blind", amount);
+    }
+
+}
