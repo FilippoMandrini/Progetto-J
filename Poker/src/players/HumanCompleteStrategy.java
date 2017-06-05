8362345671eb7d3bc01a77a99a774332275799e5
Luca Stanzani <Nickelsilver@10.87.171.56>
2017-06-06 00:36:09 +0200

    Implementato out della stringa json

--- Poker/src/players/HumanCompleteStrategy.java
+++ Poker/src/players/HumanCompleteStrategy.java
@@ -12,6 +12,7 @@ import java.util.Observable;
 import java.util.Set;
 import poker.Board;
 import poker.GameObservable;
+import server.JSONEncoder;
 
 /**
  *
@@ -19,58 +20,62 @@ import poker.GameObservable;
  */
 public class HumanCompleteStrategy extends HumanStrategy{
 
+    private JSONEncoder encoder;
+    
     public HumanCompleteStrategy(Socket socket) {
         super(socket);
+        this.encoder = JSONEncoder.getInstance();
     }
     
     @Override
     public Action act(int minBet, int bet, Set<ActionSet> allowedActions) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        out.println(encoder.encodeAct(minBet, bet, allowedActions));
+        return new HumanTestStrategy().act(minBet, bet, allowedActions);
     }
 
     @Override
     public void boardUpdated(Board board) {
-
+        out.println(encoder.encodeBoardUpdated(board));
     }
 
     @Override
     public void playerUpdated(Player player) {
-
+        out.println(encoder.encodePlayerUpdated(player));
     }
 
     @Override
     public void messageUpdated(String message) {
-
+        out.println(encoder.encodeMessageUpdated(message));
     }
 
     @Override
     public void gameStarted(GameObservable game) {
-
+        //    
     }
 
     @Override
     public void handStarted(Player dealer) {
-
+        out.println(encoder.encodeHandStarted(dealer));
     }
 
     @Override
     public void currentPlayerUpdated(Player currentPlayer) {
-
+        out.println(encoder.encodeCurrentPlayerUpdated(currentPlayer));
     }
 
     @Override
     public void bettingUpdated(int bet, int minBet, int totalPot) {
-
+        out.println(encoder.encodeBettingUpdated(bet, minBet, totalPot));
     }
 
     @Override
     public void selfUpdated(Player player) {
-
+        out.println(encoder.encodeSelfUpdated(player));
     }
 
     @Override
     public void currentPlayerActed(ShadowPlayer shadowCopy) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        out.println(encoder.encodeCurrentPlayerActed(shadowCopy));
     }
     
 }
