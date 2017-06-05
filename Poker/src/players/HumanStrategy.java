c0a917cc7b529be600a6db1da0eeefe7dae68ebf
Luca Stanzani <Nickelsilver@10.87.171.56>
2017-06-06 00:36:24 +0200

    aggiunta inizializzazione degli stream

--- Poker/src/players/HumanStrategy.java
+++ Poker/src/players/HumanStrategy.java
@@ -7,9 +7,15 @@ package players;
 
 import actions.Action;
 import actions.ActionSet;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Observable;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,9 +25,16 @@ public abstract class HumanStrategy implements Client {
 
     
     protected Socket socket;
+    protected Scanner in = null;
+    protected PrintStream out = null;
 
     public HumanStrategy(Socket socket) {
         this.socket = socket;
+        try {
+            in = new Scanner(new InputStreamReader(socket.getInputStream()));
+            out = new PrintStream(socket.getOutputStream(), true);
+        } catch (IOException ex) {
+        }   
     }
     
     
