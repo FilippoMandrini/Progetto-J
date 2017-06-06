f7db6cb5def01b296996524e2fac9a474627d0ac
cl427957 <cl427957@lib-018>
2017-06-06 15:59:01 +0200

    Migliorato encoder e decoder e iniziata implementazione estensiva

--- PokerClient/PokerClient/src/model/Game.java
+++ PokerClient/PokerClient/src/model/Game.java
@@ -18,7 +18,7 @@ public class Game {
     private List<Player> players;
     private List<Player> activePlayers;
     private Action currentAction;
-    private List<Card> communityCards;
+    private Board board;
     private int bet;
     private int minBet;
     private int totalPot;
@@ -33,9 +33,79 @@ public class Game {
     {
         for(Player player : players)
         {
-            
+            if (player.isActive())
+            {
+                if (!players.contains(player))
+                    activePlayers.add(player);
+            }
         }
     }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public List<Player> getActivePlayers() {
+        return activePlayers;
+    }
+
+    public void setActivePlayers(List<Player> activePlayers) {
+        this.activePlayers = activePlayers;
+    }
+
+    public Action getCurrentAction() {
+        return currentAction;
+    }
+
+    public void setCurrentAction(Action currentAction) {
+        this.currentAction = currentAction;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public int getBet() {
+        return bet;
+    }
+
+    public void setBet(int bet) {
+        this.bet = bet;
+    }
+
+    public int getMinBet() {
+        return minBet;
+    }
+
+    public void setMinBet(int minBet) {
+        this.minBet = minBet;
+    }
+
+    public int getTotalPot() {
+        return totalPot;
+    }
+
+    public void setTotalPot(int totalPot) {
+        this.totalPot = totalPot;
+    }
+
+    public GameType getSettings() {
+        return settings;
+    }
+
+    public void setSettings(GameType settings) {
+        this.settings = settings;
+    }
+    
+    
     
     
     
