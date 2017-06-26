218bf7b5b7f74707e388257ab65b9378deda0e0e
Luca Stanzani <Nickelsilver@10.87.171.56>
2017-06-27 01:31:30 +0200

    Aggiunto set di funzionalità all'interfaccia client per verificare connessione

--- Poker/src/players/Client.java
+++ Poker/src/players/Client.java
@@ -2,6 +2,8 @@ package players;
 
 import actions.Action;
 import actions.ActionSet;
+import java.io.IOException;
+import java.net.SocketTimeoutException;
 import java.util.Set;
 import poker.GameObserver;
 
@@ -16,7 +18,18 @@ public interface Client extends GameObserver{
      * @param bet la scommessa
      * @param allowedActions la lista delle azioni consentite
      * @return l'azione compiuta
+     * @throws java.io.IOException
+     * @throws java.net.SocketTimeoutException
      */
-    public Action act(int minBet, int bet, Set<ActionSet> allowedActions);
+    public Action act(int minBet, int bet, Set<ActionSet> allowedActions) throws IOException, SocketTimeoutException;
+    
+    public boolean isReachable(int timeout);
+    
+    public boolean isConnected();
+    
+    public void setConnected(boolean connected);
+        
+    @Override
+    public void disconnect();
     
 }
