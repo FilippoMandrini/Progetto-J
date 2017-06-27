5088f0e97a45fe0e0001a7edc9ae36287f0acd64
Luca Stanzani <Nickelsilver@10.87.171.56>
2017-06-27 12:54:36 +0200

    Aggiunto controllo tramite ping della connessione

--- Poker/src/server/ConnectionChecker.java
+++ Poker/src/server/ConnectionChecker.java
@@ -7,9 +7,11 @@ package server;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import players.Client;
-import players.HumanStrategy;
 import players.Player;
 import poker.Game;
 
@@ -21,35 +23,66 @@ public class ConnectionChecker implements Runnable {
     
     private List<Client> clients;
 
-    public ConnectionChecker(Game game) {
+    public ConnectionChecker(Game game) 
+    {
         clients = new ArrayList<>();
-        for (Player player : game.getPlayers()) {
+        for (Player player : game.getPlayers()) 
+        {
             clients.add(player.getClient());
         }
     }
 
     @Override
-    public void run() {
-        try {
-            checkConnections();
-        } catch (IOException ex) {
-        }
+    public void run() 
+    {
+        checkConnections();
     }
 
-    private void checkConnections() throws IOException {
-        while (true) {
-            for (Client client : clients) {
-                if (!client.isReachable(2000)) {
-                    client.setConnected(false);
+    private void checkConnections() 
+    {
+        while (true) 
+        {        
+            updateClients();
+            for (int i = 0; i<clients.size(); i++) 
+            {
+                if (!clients.get(i).isReachable(2000)) 
+                {
+                    clients.get(i).setConnected(false);
+                    clients.remove(i);
+                }
+                else if (!clients.get(i).isBlocked())
+                {
+                    try 
+                    {
+                        clients.get(i).ping();
+                    } 
+                    catch (IOException ex) 
+                    {
+                        System.out.println("[DISCONNESSIONE] " + ex.getMessage());
+                        clients.get(i).setConnected(false);
+                    }
                 }
             }
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException ex) {
+            try 
+            {
+                Thread.sleep(1000);
+            } 
+            catch (InterruptedException ex) {
             }
         }
     }
     
-    
-    
+    public void updateClients()
+    {
+        ArrayList<Client> connected = new ArrayList<>();
+        for(Iterator<Client> iter = clients.iterator(); iter.hasNext();)
+        {
+            Client client = iter.next();
+            if (client.isConnected())
+            {
+                connected.add(client);
+            }
+        }
+        clients = connected;
+    }
 }
